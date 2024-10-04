db7f7b5f63b7e9d562f65803cda0b2cb5e809dee
Mira Anand <miraanand@comcast.net>
2024-10-04 13:26:06 -0700

    10 minute update - 3

--- Lab18MiraAnand.cpp
+++ Lab18MiraAnand.cpp
@@ -32,11 +32,13 @@ int main()
     return 0;
 }
 
-// function header
-// DESCRIPTION:
-// ARGUMENTS:
+// addNodeToHead(MovieReviewNode *& head, double rating, string comment) function header
+// DESCRIPTION: this function will create a new node, set the value of the new node, and add this new node to the front (head) of the list
+// ARGUMENTS: MovieReviewNode *& head, which is a pointer to the head of the list
+// - double rating, which represents the movie rating being added
+// - string comment, which represents the movie comment being added
 // Passing by reference because the linked list will be modified and this modification will also reflect in main()
-// RETURNS:
+// RETURNS: nothing, void function. Purpose is to just create a node with values and add it to the head of the list
 void addNodeToHead(MovieReviewNode *& head, double rating, string comment)
 {
     MovieReviewNode *newNode = new MovieReviewNode; // create a new node
@@ -52,5 +54,34 @@ void addNodeToHead(MovieReviewNode *& head, double rating, string comment)
     {
         newNode->next = head; // next will now point to list's 1st node
         head = newNode; // head points to newNode
+    }
+}
+
+// addNodeToTail(MovieReviewNode *& head, double rating, string comment) function header
+// DESCRIPTION:
+// ARGUMENTS:
+// Passing by reference because the linked list will be modified and this modification will also reflect in main()
+// RETURNS:
+void addNodeToTail(MovieReviewNode *& head, double rating, string comment)
+{
+    MovieReviewNode *newNode = new MovieReviewNode; // create a new node
+    newNode->movieRating = rating; // set movie rating of new node
+    newNode->movieComment = comment; // set movie comment of new node
+    newNode->next = nullptr; // set next pointer to nullptr
+
+    if (!head) // if linked list is empty
+    {
+        head = newNode; // make newNode the head
+    }
+    else // if linked list is NOT empty
+    {
+        MovieReviewNode *current = head; // current points to head
+
+        while (current->next) // loop to traverse list to the end
+        {
+            current = current->next; // current points to next node in the list
+        }
+
+        current->next = newNode; // last node is now set to newNode
     }
 }
